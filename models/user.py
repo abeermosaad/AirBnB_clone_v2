dc4b793b47523abf816bd5d392a75f01db4dc31a
Abodawoud <yousefabodawoud@gmail.com>
2023-09-21 00:50:12 +0300

    Edit City Place User

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
-
+from models.place import Place
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -11,7 +11,7 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship("Place", back_populates="user",
                           cascade="all, delete-orphan")
