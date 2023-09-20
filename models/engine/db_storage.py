6d007ef0107507c6536f0a45dbc0e01f366fe864
abeermosaad <abeermosaad69@gmail.com>
2023-09-20 22:48:02 +0300

    Task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""sqlalchemy module"""
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+import os
+from models.base_model import BaseModel, Base
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class DBStorage():
+    """DBStorage class"""
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            os.environ['HBNB_MYSQL_USER'],
+            os.environ['HBNB_MYSQL_PWD'],
+            os.environ['HBNB_MYSQL_HOST'],
+            os.environ['HBNB_MYSQL_DB']), pool_pre_ping=True)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = Session()
+        if os.environ['HBNB_ENV'] == 'test':
+            metadata = MetaData(bind=self.__engine)
+            metadata.drop_all()
+
+    def all(self, cls=None):
+        from models import storage
+        dic = {}
+        if cls is None:
+            result = self.__session.query(
+                State, User, City, Amenity, Place, Review).all()
+            for r in result:
+                dic[f"{r.__class__.__name__}.{r.id}"] = r
+            return (dic)
+        else:
+            result = self.__session.query(eval(cls)).all()
+            for r in result:
+                dic[f"{r.__class__.__name__}.{r.id}"] = r
+            return (dic)
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        from models.base_model import BaseModel, Base
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+        Base.metadata.create_all(self.__engine)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    # name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        from models import storage
+        dic = storage.all('City')
+        return [].append(v for k, v in dic.items() if self.id == v['state_id'])
